ef967a8291be2b176f02514b5cec2cfda50ea65d
Haritha Mandanapu <haritha.m22@gmail.com>
2025-08-01 16:46:16 -0500

    Updates

---
+++ assets/skills/index.ts
@@ -0,0 +1,71 @@
+// Skill images imports
+import javascriptImg from './javascript.png';
+import typescriptImg from './typescript.png';
+import reactImg from './react.png';
+import angularImg from './angular.png';
+import reduxImg from './redux.png';
+import nodejsImg from './nodejs.png';
+import graphqlImg from './graphql.png';
+import nextjsImg from './nextjs.png';
+import tailwindImg from './tailwind.png';
+import bootstrapImg from './bootstrap.png';
+import materialUiImg from './material-ui.png';
+import mongodbImg from './mongodb.png';
+import mysqlImg from './mysql.png';
+import postcssImg from './postcss.svg';
+import jestImg from './jest.png';
+import lessImg from './less.png';
+import sassImg from './sass.png';
+import webpackImg from './webpack.png';
+import babelImg from './babel.png';
+import gitImg from './git.png';
+import storybookImg from './storybook.png';
+import reactTestingLibraryImg from './react-testing-library.png';
+import seleniumImg from './selenium.png';
+import cypressImg from './cypress.png';
+import playwrightImg from './playwright.png';
+import pythonImg from './python.png';
+import javaImg from './java.png';
+import kotlinImg from './kotlin.png';
+import springBootImg from './spring-boot.png';
+import dockerImg from './docker.png';
+import kubernetesImg from './kubernetes.png';
+import awsImg from './aws.png';
+import jenkinsImg from './jenkins.png';
+
+export const skillImages = {
+  javascript: javascriptImg,
+  typescript: typescriptImg,
+  react: reactImg,
+  angular: angularImg,
+  redux: reduxImg,
+  nodejs: nodejsImg,
+  graphql: graphqlImg,
+  nextjs: nextjsImg,
+  tailwind: tailwindImg,
+  bootstrap: bootstrapImg,
+  'material-ui': materialUiImg,
+  mongodb: mongodbImg,
+  mysql: mysqlImg,
+  postcss: postcssImg,
+  jest: jestImg,
+  less: lessImg,
+  sass: sassImg,
+  webpack: webpackImg,
+  babel: babelImg,
+  git: gitImg,
+  storybook: storybookImg,
+  'react-testing-library': reactTestingLibraryImg,
+  selenium: seleniumImg,
+  cypress: cypressImg,
+  playwright: playwrightImg,
+  python: pythonImg,
+  java: javaImg,
+  kotlin: kotlinImg,
+  'spring-boot': springBootImg,
+  docker: dockerImg,
+  kubernetes: kubernetesImg,
+  aws: awsImg,
+  jenkins: jenkinsImg,
+};
+
